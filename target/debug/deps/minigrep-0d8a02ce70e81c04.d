2d8f0e5de8182cc24f5cf8a153e762210854a204
Daniel Espejel <duedme@gmail.com>
2022-07-04 23:52:16 -0500

    App: started building CLI

---
+++ target/debug/deps/minigrep-0d8a02ce70e81c04.d
@@ -0,0 +1,5 @@
+/Users/Daniel/Documents/Rust/Projects/Minigrep/minigrep/target/debug/deps/minigrep-0d8a02ce70e81c04: src/main.rs
+
+/Users/Daniel/Documents/Rust/Projects/Minigrep/minigrep/target/debug/deps/minigrep-0d8a02ce70e81c04.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/Users/Daniel/Documents/Rust/Projects/Minigrep/minigrep/target/debug/minigrep: /Users/Daniel/Documents/Rust/Projects/Minigrep/minigrep/src/main.rs
